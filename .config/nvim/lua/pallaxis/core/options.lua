01e19afa4aad34cff6a9e8b18f110bd1863a57c7
Pallaxis <sam_e3u@hotmail.com>
2024-04-20 01:31:59 +1200

    adding and changing nvim configs

--- .config/nvim/lua/pallaxis/core/options.lua
+++ .config/nvim/lua/pallaxis/core/options.lua
@@ -3,5 +3,19 @@ local opt = vim.opt
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
+opt.number = true -- line numbers 
+opt.relativenumber = true -- relative line numbers
+opt.breakindent = true
+
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 
 vim.api.nvim_set_option("clipboard", "unnamed") -- copying in wayland

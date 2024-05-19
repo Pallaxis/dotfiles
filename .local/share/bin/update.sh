a5466e84c23805d8d7f960fc8d5d513d295a0165
Pallaxis <sam_e3u@hotmail.com>
2024-05-19 21:43:13 +1200

    custom waybar + update script

--- .local/share/bin/update.sh
+++ .local/share/bin/update.sh
@@ -1,4 +1,15 @@
-printf "\n\nUpdating Arch + AUR Packages:\n\n"
+if [ "$1" == "up" ] ; then
+trap 'pkill -RTMIN+20 waybar' EXIT
+command="
+fastfetch
+$0 upgrade
 yay -Syu
-printf "\n\nUpdating Flatpak Packages:\n\n"
 flatpak update
+read -n 1 -p 'Press any key to continue...'
+"
+kitty --title systemupdate sh -c "${command}"
+fi
+#printf "\n\nUpdating Arch + AUR Packages:\n\n"
+#yay -Syu
+#printf "\n\nUpdating Flatpak Packages:\n\n"
+#flatpak update

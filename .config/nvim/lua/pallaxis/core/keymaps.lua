2c7f8df121f4c5cc95f9e00886d6aa6043826ef8
Pallaxis <sam_e3u@hotmail.com>
2024-09-28 18:40:28 +1200

    fixing tmux and moving tmux.conf to .config/tmux

--- .config/nvim/lua/pallaxis/core/keymaps.lua
+++ .config/nvim/lua/pallaxis/core/keymaps.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
 vim.g.havenerdfont = true
+
+
+
+-- tmux-navigator keybinds
+--let g:tmux_navigator_no_mappings = 1
+--nnoremap <silent> <Up> :<C-U>TmuxNavigateLeft<cr>
+--nnoremap <silent> <Down> :<C-U>TmuxNavigateDown<cr>
+--nnoremap <silent> <Left> :<C-U>TmuxNavigateUp<cr>
+--nnoremap <silent> <Right> :<C-U>TmuxNavigateRight<cr>
+

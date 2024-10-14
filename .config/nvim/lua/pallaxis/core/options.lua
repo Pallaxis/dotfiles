030ea39782f0bffe35e44ab7bddd84da02c1a99c
Pallaxis <sam_e3u@hotmail.com>
2024-10-14 23:14:09 +1300

    unfucking hyprland.conf and changing vim tabstop to 4

--- .config/nvim/lua/pallaxis/core/options.lua
+++ .config/nvim/lua/pallaxis/core/options.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.number = true -- line numbers 
 opt.relativenumber = true -- relative line numbers

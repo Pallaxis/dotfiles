c61456d98330c8baefe37403fb01e34f143dbe87
Pallaxis <sam_e3u@hotmail.com>
2024-11-14 23:04:14 +1300

    added hex colorizing plugin to nvim, minor formatting changes to hypr

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,2 @@
 require("pallaxis.core")
 require("pallaxis.lazy")
-vim.api.nvim_set_option("clipboard", "unnamed")
--- .config/nvim/lua/pallaxis/core/options.lua
+++ .config/nvim/lua/pallaxis/core/options.lua
@@ -7,18 +7,17 @@ opt.number = true -- line numbers
 opt.relativenumber = true -- relative line numbers
 opt.breakindent = true
 
+local api = vim.api
+api.nvim_set_option("clipboard", "unnamedplus")
 
 -- Highlight when yanking
-vim.api.nvim_create_autocmd('TextYankPost', {
+api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
-
-
-vim.api.nvim_set_option("clipboard", "unnamed") -- copying in wayland
 
 vim.filetype.add {
   extension = { rasi = 'rasi' },
